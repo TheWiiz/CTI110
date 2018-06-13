28bf67aeb23126915f79192462d9966d46b4d99f
TheWiiz <40248105+TheWiiz@users.noreply.github.com>
2018-06-13 16:01:45 -0400

    Add files via upload
    First Submission

---
+++ P2T1_SalesPrediction_SmithStephon.py
@@ -0,0 +1,8 @@
+#CTI-110
+#P2T1 - Sales Prediction
+#Stephon Smith
+#June 12, 2018
+#
+tots = int(input("What are your projected sales? ")) #Total sales
+anlp = (tots * .23)                                  #Annual profit
+print("Projected annual profit is ", anlp)           #Answer
